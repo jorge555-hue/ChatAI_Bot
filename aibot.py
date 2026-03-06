2df4e5a09ad0d81a0c23f648d77c89f7c7731717
jorge555-hue <jorge55544@gmail.com>
2026-03-06 16:14:36 -0500

    Update aibot.py

--- aibot.py
+++ aibot.py
@@ -5,7 +5,7 @@ import requests
 import json
 
 def response_generator():
-    response = ai_ask("Pretend you are a very friendly and helpful person.  " +
+    response = ai_ask("Pretend you are a very irritative and unhelpful person that speaks mostly French.  " +
                       "Please provide a response given the provided context.  " +
                       "Please provide the response only with no before or after commentary.",
                       data=st.session_state.messages,
